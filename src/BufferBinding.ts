7959bf485dd51a1b0a8a0b4d562c662148e757a6
Yukai Huang <yukaihuangtw@gmail.com>
2018-02-16 19:53:11 +0800

    Resolve edit conflicts with PQueue

--- src/BufferBinding.ts
+++ src/BufferBinding.ts
@@ -2,6 +2,7 @@ import * as fs from 'fs';
 import * as vscode from 'vscode';
 
 import BufferProxy from '@atom/teletype-client/lib/buffer-proxy';
+import * as PQueue from 'p-queue';
 
 interface Position {
   row: number;
@@ -25,12 +26,15 @@ export default class BufferBinding {
   private bufferProxy : BufferProxy;
   public didDispose : Function;
   private updatingText : boolean;
+  private editsQueue : PQueue;
+  private updateTextSync : Function;
 
   constructor ({buffer, isHost, didDispose}) {
     this.buffer = buffer;
     this.isHost = isHost;
     this.didDispose = didDispose;
     this.updatingText = false;
+    this.editsQueue = new PQueue({concurrency: 1});
   }
 
   dipose () {
@@ -54,16 +58,7 @@ export default class BufferBinding {
   }
 
   updateText (textUpdates: TextUdpate[]) {
-    this.editor.edit(builder => {
-      this.updatingText = true
-      for (let i = textUpdates.length - 1; i >= 0; i--) {
-        const {oldStart, oldEnd, newText} = textUpdates[i]
-        builder.replace(this.createRange(oldStart, oldEnd), newText)
-      }
-
-    }, { undoStopBefore: false, undoStopAfter: true }).then(() => {
-      this.updatingText = false
-    })
+    this.editsQueue.add(() => this.updateTextPromise.bind(this)(textUpdates))
   }
 
   private createRange (start : Position, end : Position) : vscode.Range {
@@ -86,6 +81,21 @@ export default class BufferBinding {
         )
       })
     }
+  }
+
+  private updateTextPromise (textUpdates: TextUdpate[]) : Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.updatingText = true
+      this.editor.edit(builder => {
+        for (let i = textUpdates.length - 1; i >= 0; i--) {
+          const {oldStart, oldEnd, newText} = textUpdates[i]
+          builder.replace(this.createRange(oldStart, oldEnd), newText)
+        }
+      }, { undoStopBefore: false, undoStopAfter: true }).then(() => {
+        this.updatingText = false
+        resolve()
+      })
+    })
   }
 
   save () {
